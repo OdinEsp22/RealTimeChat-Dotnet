3fda2f29d790cd8ee0cccb607f907afbc1b2b587
OdinEsp22 <juan_4002@hotmail.com>
2024-06-28 13:52:11 -0600

    Add MediatR to use cases

---
+++ src/UseCases/Interfaces/Icommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace UseCases.Interfaces;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TResponse"></typeparam>
+public interface ICommand<out TResponse> : IRequest<TResponse>  { }
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TRequest"></typeparam>
+/// <typeparam name="TResponse"></typeparam>
+public interface ICommandHadler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : ICommand<TResponse> { }
